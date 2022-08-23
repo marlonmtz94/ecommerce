bc151ef2b6068356671b1222a75a89a6468fe2fe
Marlon <marlon.marlo@hcl.com>
2022-08-23 09:58:57 -0500

    updating with some iteration 3partss

---
+++ src/main/java/com/marlon/model/ShoppingCart.java
@@ -0,0 +1,44 @@
+package com.marlon.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="shoppingCart")
+public class ShoppingCart {
+	
+	
+	@Id @Column(name="cartid")
+	private int cartid; 
+	
+	@Column(name="productId")
+	private int productId; 
+	
+	
+	
+	
+	
+	
+}
